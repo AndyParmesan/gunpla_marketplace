44667e66d22b941f2efde8723059ffd8bebce776
AndyParmesan <andyparmesan12Gmail.com>
2025-01-02 18:51:06 +0800

    heck

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import{
+  BrowserRouter,
+  Routes,
+  Route,
+
+} from "react-router-dom";
+import Gunpla from "./pages/Gunpla";
+import Update from "./pages/Update";
+import Add from "./pages/Add";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Gunpla/>}></Route>
+        <Route path="/Add" element= {<Add/>}></Route>
+        <Route path="/Update" element= {<Update/>}></Route>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/Gunpla.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Gunpla =()=>{
+    return(
+        <div> Gunpla</div>
+    )
+}
+
+export default Gunpla
